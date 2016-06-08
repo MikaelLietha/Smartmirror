2603bc432701f203da718d52860b007835c07389
MikaelLietha <Mikael.Lietha@gmail.com>
2016-06-08 20:10:43 +0200

    added functionality for pi

--- Smartmirror/settings.py
+++ Smartmirror/settings.py
@@ -106,7 +106,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/mikael/Smartmirror/Smartmirror/Templates"
+    #"/home/mikael/Smartmirror/Smartmirror/Templates"
+    "/home/pi/Smartmirror/Smartmirror/Templates"
 )
 
 INSTALLED_APPS = (
